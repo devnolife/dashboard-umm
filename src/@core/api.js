189d5f48dec55aa84bf3ddf4c9a84233a6e6920f
devnolife <andi_agung@student.unismuh.ac.id>
2024-06-15 17:18:48 +0800

    change url

--- src/@core/api.js
+++ src/@core/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const baseUrl2 = 'http://localhost:8000/api'
-export const baseUrl = 'http://192.168.1.53:8080/api'
+export const baseUrl = 'https://api.beasiswa.unismih.ac.id/api'
 import toast from 'react-hot-toast'
 
 export async function fetch(method, url, params, body) {
