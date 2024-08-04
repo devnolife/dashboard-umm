cdb105d9f8b83f7575fee4f92dcc8838656fb5ad
devnolife <andi_agung@student.unismuh.ac.id>
2024-08-05 04:05:18 +0800

    fix: fix typo in Content-Type header value

--- src/@core/api.js
+++ src/@core/api.js
@@ -5,7 +5,7 @@ import toast from 'react-hot-toast'
 
 export async function fetch(method, url, params, body) {
   try {
-    axios.defaults.headers.post['Content-Type'] = 'application/json'
+    axios.defaults.headers.post['Content-Type'] = 'application/ json'
     return await axios[method](baseUrl + url, params, body)
   } catch (e) {
     toast.error(e.response.data.message)
